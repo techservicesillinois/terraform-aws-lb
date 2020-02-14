689f42cda0a699a069a3732a3ed6c6081998cf22
David D. Riddle <ddriddle@illinois.edu>
2020-02-14 13:47:50 -0600

    Fixup

---
+++ test/list-buckets.py
@@ -0,0 +1,10 @@
+import boto3
+
+# Retrieve the list of existing buckets
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+
+# Output the bucket names
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
